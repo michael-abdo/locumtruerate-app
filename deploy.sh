a4ad0c4baa7e6108f8e4c94a85facfc5f8826e8f
Michael-abdo <michael-abdo@users.noreply.github.com>
2025-07-13 00:57:22 +0000

    🔧 Fix production URL mismatch - use correct app name

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ REPO="michael-abdo/locumtruerate-app"
 STAGING_APP="locumtruerate-staging"
 STAGING_URL="https://locumtruerate-staging-66ba3177c382.herokuapp.com"
 PRODUCTION_APP="locumtruerate-production"
-PRODUCTION_URL="https://locumtruerate-production-66ba3177c382.herokuapp.com"
+PRODUCTION_URL="https://locumtruerate-production.herokuapp.com"
 
 # Function to show usage
 show_usage() {
