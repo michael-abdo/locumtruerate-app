91b281fe3b451a4f750e392b5824f67b2424c328
Michael-abdo <michael-abdo@users.noreply.github.com>
2025-07-13 01:00:49 +0000

    🎯 Use unique production app name to avoid conflicts

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,8 @@ NC='\033[0m'
 REPO="michael-abdo/locumtruerate-app"
 STAGING_APP="locumtruerate-staging"
 STAGING_URL="https://locumtruerate-staging-66ba3177c382.herokuapp.com"
-PRODUCTION_APP="locumtruerate-production"
-PRODUCTION_URL="https://locumtruerate-production.herokuapp.com"
+PRODUCTION_APP="locumtruerate-prod-demo"
+PRODUCTION_URL="https://locumtruerate-prod-demo.herokuapp.com"
 
 # Function to show usage
 show_usage() {
